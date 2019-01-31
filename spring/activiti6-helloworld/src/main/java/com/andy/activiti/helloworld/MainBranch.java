96692efc6914ba934e81d6a0b269f5a57f04894a
andyluna <xiangdan311@163.com>
2019-01-31 16:05:07 +0800

    主分支上新增了一个类

---
+++ spring/activiti6-helloworld/src/main/java/com/andy/activiti/helloworld/MainBranch.java
@@ -0,0 +1,14 @@
+package com.andy.activiti.helloworld;
+
+/**
+ * @projectName: springstudy  com.andy.activiti.helloworld
+ * @desc:
+ * @author: xiangdan
+ * @time : 2019-01-31 Thursday 16:02
+ */
+public class MainBranch {
+	public static void main(String[] args){
+		System.out.println("主分支");
+	}
+
+}
